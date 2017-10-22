be8985bad7e1e0bf1daf82d6645287ddf9dd8ca5
Matthew Leibowitz <mattleibow@live.com>
2017-10-22 06:32:09 +0200

    Updated the sample

--- InfiniteScrollingApp/InfiniteScrollingApp/App.xaml.cs
+++ InfiniteScrollingApp/InfiniteScrollingApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace InfiniteScrollingApp
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart()
--- InfiniteScrollingApp/InfiniteScrollingApp/ViewModels/MainViewModel.cs
+++ InfiniteScrollingApp/InfiniteScrollingApp/ViewModels/MainViewModel.cs
@@ -1,15 +1,42 @@
-﻿using Xamarin.Forms;
+﻿using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+using InfiniteScrolling;
 
 namespace InfiniteScrollingApp
 {
 	public class MainViewModel : BindableObject
 	{
+		private const int PageSize = 20;
+
 		public static readonly BindableProperty IsWorkingProperty =
 			BindableProperty.Create(nameof(IsWorking), typeof(bool), typeof(MainViewModel), default(bool));
 
 		public MainViewModel()
 		{
-			Items = new InfiniteDataCollection();
+			var dataSource = new FakeDataSource();
+
+			Items = new InfiniteScrollCollection<DataItem>
+			{
+				OnLoadMore = async () =>
+				{
+					// load the next page
+					var page = Items.Count / PageSize;
+					var items = await dataSource.GetItemsAsync(page + 1, PageSize);
+					return items.Select(item => new DataItem(item));
+				}
+			};
+
+			RefreshCommand = new Command(() =>
+			{
+				// clear and start again
+				Items.Clear();
+				Items.LoadMoreAsync();
+			});
+
+			// load the initial data
+			Items.LoadMoreAsync();
 		}
 
 		public bool IsWorking
@@ -18,6 +45,8 @@ namespace InfiniteScrollingApp
 			set { SetValue(IsWorkingProperty, value); }
 		}
 
-		public InfiniteDataCollection Items { get; }
+		public InfiniteScrollCollection<DataItem> Items { get; set; }
+
+		public ICommand RefreshCommand { get; }
 	}
 }
